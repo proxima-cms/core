1ed2a787fa34dd27bf932fd647d7815329ff4050
Richard <willis.rh@gmail.com>
2011-09-29 12:58:42 +0100

    Removed assets files from admin module

--- modules/admin/views/admin/page/assets_popup/download_html.php
+++
@@ -1,4 +0,0 @@
-<span class="asset-download asset-<?php echo $asset->mimetype->type?>">
-	<a href="<?php echo $asset->url(TRUE)?>">Download <?php echo $asset->filename?></a> 
-	(<?php echo Text::bytes($asset->filesize)?>)
-</span>
