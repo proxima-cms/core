18225a08ca888e5f58670b17db16ac135b903df6
Richard <willis.rh@gmail.com>
2011-09-29 14:31:38 +0100

    Removed old base backup

--- modules/base/baseold/classes/model/asset.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Model_Asset extends Model_Base_Asset { }
--- modules/base/baseold/classes/model/base/config.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-/*
- * Base config model
- */
-class Model_Base_Config extends Model_Base {
-
-	public $_table_name = 'config';		
-	
-}
