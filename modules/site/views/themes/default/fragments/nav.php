487145a36a04e82359dea2bd88b0cf75484cb372
Richard <willis.rh@gmail.com>
2011-09-30 13:29:18 +0100

    Removed nav fragment view

--- modules/site/views/themes/default/fragments/nav.php
+++
@@ -1,9 +0,0 @@
-<h2>Nav</h2>
-
-<ul>
-<?php
-$pages = ORM::factory('page')->where('parent_id', '=', 0)->find_all();
-foreach($pages as $page){?>
-	<li><?php echo HTML::anchor($page->uri, $page->title)?></li>
-<?php }?>
-</ul>
