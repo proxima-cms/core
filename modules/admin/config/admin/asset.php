b15b0d8c912f14a9b00919498b6dfca1303cb336
Richard <willis.rh@gmail.com>
2011-10-03 23:49:47 +0100

    Removed moved files

--- modules/admin/config/admin/asset.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-	'upload_path' 		=> 'media/assets',
-	'max_file_uploads' 	=> 5
-);
--- modules/admin/views/admin/page/users/tree/users_item.php
+++
@@ -1,3 +0,0 @@
-<li>
-	<?php echo HTML::anchor('admin/users/edit/'.$user->id, $user->username, array('class' => 'user'))?>
-</li>
