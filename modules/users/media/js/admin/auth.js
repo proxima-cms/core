c1bfbf0ce6ca1d46b21f62edd59b8065010a416b
Richard <willis.rh@gmail.com>
2011-10-07 14:02:58 +0100

    Added swifmailer vendor package and auth JS

---
+++ modules/users/media/js/admin/auth.js
@@ -0,0 +1,21 @@
+/*
+ *
+ * @filename : admin.controllers.js
+ * @developer : badsyntax.co
+ *
+ */
+(function(window, $, Admin){
+	
+	if (!Admin) return;
+	
+	var cons = Admin.cons;
+	
+	Admin.controller.auth = {
+		
+		action_index: function(){
+		
+		}
+	
+	};
+		
+})(this, this.jQuery, this.Admin);
