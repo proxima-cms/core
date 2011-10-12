e91e0742912e39b7ef2ef44877cb67afa5b26592
Richard <willis.rh@gmail.com>
2011-10-12 21:39:01 +0100

    Added styles() and scripts() helper functions

---
+++ modules/core/classes/html.php
@@ -0,0 +1,39 @@
+<?php
+
+abstract class HTML extends Kohana_HTML {
+
+	public static function styles($styles = array())
+	{
+		if (!is_array($styles))
+		{
+			throw new Exception('Array expected');
+		}
+
+		$styles_html = '';
+
+		foreach($styles as $style)
+		{
+			$styles_html .= HTML::style($style);
+		}
+
+		return $styles_html;
+	}
+	
+	public static function scripts($scripts = array())
+	{
+		if (!is_array($scripts))
+		{
+			throw new Exception('Array expected');
+		}
+
+		$scripts_html = '';
+
+		foreach($scripts as $script)
+		{
+			$scripts_html .= HTML::script($script);
+		}
+
+		return $scripts_html;
+	}
+
+}
