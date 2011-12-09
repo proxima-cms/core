5d23d40a93e70e2c7916803d08bd79774ad9f14d
Richard <willis.rh@gmail.com>
2011-12-09 21:53:05 +0000

    Added component module

---
+++ proxima/core/component/config/component/details.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'name'        => 'Components',
+	'description' => 'Core site components',
+	'author'      => 'Richard Willis',
+	'admin_nav'   => array('controller' => 'components', 'name' => NULL),
+	'load_order'  => 7
+);
