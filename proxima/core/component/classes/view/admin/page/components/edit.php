d3b5b099f3ae8e22e3a64182cff84a24eb91c82b
Richard <willis.rh@gmail.com>
2011-12-10 14:22:12 +0000

    Removed unused junk from component model views

--- proxima/core/component/classes/view/admin/page/components/edit.php
+++ proxima/core/component/classes/view/admin/page/components/edit.php
@@ -2,27 +2,4 @@
 
 class View_Admin_Page_Components_Edit extends View_Model {
 	
-	public function __construct($file = NULL, array $data = NULL)
-	{
-		parent::__construct($file, $data);
-	}
-	
-	// Return the total amount of filtered items
-	public function var_total()
-	{
-		//return ORM::factory($this->model)->count_all();
-	}
-
-
-	// Return the pagination
-	public function var_pagination()
-	{
-		//return Pagination::factory(array(
-		//	'total_items'    => $this->view->total,
-		//	'items_per_page' => $this->view->items_per_page,
-		//	'view'           => 'admin/pagination/links'
-		//)); 
-	}
-
-
 }
