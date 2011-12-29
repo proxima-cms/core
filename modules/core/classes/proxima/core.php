6b7fba78a423ccc809b9c27a8de32754f311aa4f
Richard <willis.rh@gmail.com>
2011-12-29 16:19:00 +0000

    Routes are now set within core init method

--- modules/core/classes/proxima/core.php
+++ modules/core/classes/proxima/core.php
@@ -20,6 +20,9 @@ class Proxima_Core {
 
 		// Attach the database config reader.
 		Kohana::$config->attach(new Config_Database);
+
+		// Set the application routes.
+		self::set_routes();
 	}
 
 	/**
@@ -89,7 +92,6 @@ class Proxima_Core {
 	
 	/**
 	* Set the application routes.
-	* This method is called once within core/site/init.php
 	*
 	* @return  void
 	*/
@@ -97,6 +99,106 @@ class Proxima_Core {
 	{
 		if ( ! Route::cache())
 		{
+			/*
+			 * Admin routes 
+			 */
+			// Admin controllers
+			Route::set('admin', 'admin(/<controller>(/<action>(/<id>)))')
+				->defaults(array(
+					'action'     => 'index',
+					'directory'  => 'admin',
+					'controller' => 'home'
+				)); 
+
+			/*
+			 * Asset routes 
+			 */
+			// Admin media
+			Route::set('admin/media', 'admin/media(/<file>)', array('file' => '.+'))
+				->defaults(array(
+					'controller' 	=> 'media',
+					'directory'	=> 'admin',
+					'file'       	=> NULL,
+				));
+				
+			// Admin Assets - get asset
+			Route::set('admin/get-asset', 'admin/assets/get_asset(/<id>)(/<width>)(/<height>)(/<crop>)')
+				->defaults(array(
+					'action' 	=> 'get_asset',
+					'directory' 	=> 'admin',
+					'controller' 	=> 'assets'
+				));
+				
+			// Admin Assets - get image url
+			Route::set('admin/get-asset', 'admin/assets/get_image_url/(<id>)(/<width>)(/<height>)')
+				->defaults(array(
+					'action' 		=> 'get_image_url',
+					'directory' 	=> 'admin',
+					'controller' 	=> 'assets'
+				));
+
+			// Global media assets
+			Route::set('media/assets', 'media/assets/resized/(<id>_<width>_<height>_<crop>_<filename>)', array(
+					'id' 		=> '\d+',
+					'width' 	=> '\d+',
+					'height'	=> '\d+',
+					'crop'		=> '\d+',
+					'filename'	=> '.+'
+				))
+				->defaults(array(
+					'directory'	=> 'admin',
+					'controller'	=> 'assets',
+					'action'	=> 'get_asset',
+					'id'		=> 0,
+					'height'	=> NULL,
+					'crop'		=> NULL,	
+					'filename'	=> NULL,
+				));
+
+			// Admin Assets - folders
+			Route::set('admin/assets-folders', 'admin/assets/folders(/<action>)(/<id>)')
+				->defaults(array(
+					'controller'  => 'assets_folders',
+					'directory'   => 'admin',
+				)); 
+				
+			// Admin popup assets
+			Route::set('admin/popup-assets', 'admin/assets/popup(/<action>)(/<id>)')
+				->defaults(array(
+					'controller' 	=> 'assets_popup',
+					'directory' 	=> 'admin',
+				));
+
+			/*
+			 * Modules routes 
+			 */
+			Route::set('admin/modules', 'admin/modules(/<action>)(/<module>)')
+				->defaults(array(
+					'controller' => 'modules',
+					'directory'  => 'admin',
+					'action'     => 'index',
+					'module'     => NULL,
+				));
+
+			/*
+			 * Pages routes 
+			 */
+			Route::set('admin/pages-types', 'admin/pages/types(/<action>)(/<id>)')
+				->defaults(array(
+					'controller'  => 'pages_types',
+					'directory'   => 'admin',
+				)); 
+
+			/*
+			 * Tags routes 
+			 */
+			Route::set('admin/tag-delete', 'admin/tags/delete/<id>', array('id' => '.*'))
+				->defaults(array(
+					'action'    => 'delete',
+					'directory'   => 'admin',
+					'controller'  => 'tags'
+				)); 
+
 			// Error page
 			Route::set('error', 'error/<action>(/<message>)', array('action' => '[0-9]++', 'message' => '.+'))
 				->defaults(array(
