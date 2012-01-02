f65ccf17593095da9223befdfa4729154c81fbdd
Richard <willis.rh@gmail.com>
2012-01-02 11:39:25 +0000

    Added migration to add lang field to users table. Refs #23

---
+++ application/migrations/install/20120102113535_add-lang-field-to-users-table.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') or die('No direct script access.');/**
+ * Add lang field to users table
+ */
+class Migration_Install_20120102113535 extends Minion_Migration_Base {
+
+	/**
+	 * Run queries needed to apply this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function up(Kohana_Database $db)
+	{
+		// $db->query(NULL, 'CREATE TABLE ... ');
+		$db->query(NULL, 'ALTER TABLE  `users` ADD  `lang` VARCHAR( 128 ) NULL AFTER  `password`');
+	}
+
+	/**
+	 * Run queries needed to remove this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function down(Kohana_Database $db)
+	{
+		$db->query('ALTER TABLE `users` DROP `lang`');
+	}
+}
