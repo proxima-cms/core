eed5ce70ef59683a9244a85a39e6d18ec25a1a5f
Richard Willis <willis.rh@gmail.com>
2012-01-13 21:37:32 +0000

    Code cleanup. Refs #11

--- proxima/core/classes/proxima/asset.php
+++ proxima/core/classes/proxima/asset.php
@@ -9,17 +9,18 @@ class Proxima_Asset {
 			throw new Exception('File does not exist on file system.');
 		}
 
+		// Does the asset exist on the server?
 		if (Valid::url($file_path) === TRUE)
 		{
-			try 
+			try
 			{
-				// Download the file.
+				// Download the file
 				$asset_data = Request::factory($file_path)
 					->method(Request::GET)
 					->execute();
-			}		
-			catch(Request_Exception $e) 
-			{		
+			}
+			catch(Request_Exception $e)
+			{
 				throw $e;
 			}
 
@@ -28,7 +29,7 @@ class Proxima_Asset {
 				. DIRECTORY_SEPARATOR
 				. basename($file_path);
 
-			file_put_contents($file_path, $asset_data); 
+			file_put_contents($file_path, $asset_data);
 		}
 
 		$asset = ORM::factory('asset');
@@ -37,28 +38,28 @@ class Proxima_Asset {
 
 		return $asset;
 	}
-	
+
 	public static function resize($file_in = NULL, $file_out = NULL, $width = NULL, $height = NULL, $crop = NULL)
 	{
-		if ($width AND $height)
+		if ($width !== NULL AND $height !== NULL)
 		{
 			$image = Image::factory($file_in);
 
-			if ($crop)
+			if ($crop !== NULL)
 			{
 				if (($image->width / $image->height) > ($width / $height))
 				{
 					$resized_w = ($height / $image->height) * $image->width;
-					$offset_x = round(($resized_w - $width) / 2);
-					$offset_y = 0;			
+					$offset_x  = round(($resized_w - $width) / 2);
+					$offset_y  = 0;
 					$image->resize(NULL, $height);
 				}
 				else
 				{
 					$resized_h = ($width / $image->width) * $image->height;
-					$offset_x = 0;
-					$offset_y = round(($resized_h - $height) / 2);			
-					$image->resize($width, NULL);				
+					$offset_x  = 0;
+					$offset_y  = round(($resized_h - $height) / 2);
+					$image->resize($width, NULL);
 				}
 
 				$image->crop($width, $height, $offset_x, $offset_y);
@@ -66,20 +67,20 @@ class Proxima_Asset {
 			else
 			{
 				$image->resize($width, $height);
-			}			
-			
+			}
+
 			$image->save($file_out);
-		}	
+		}
 	}
-	
-	public static function docpath($asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
+
+	public static function docpath(Model_Asset $asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
 	{
 		$pathinfo = pathinfo($asset->filename);
 
 		$filename = $pathinfo['filename'];
 
 		$crop = (string) (int) $crop;
-		
+
 		$path = Kohana::$config->load('assets.upload_path').'/'.$asset->filename;
 
 		if ($asset->mimetype->subtype == 'image' AND $width AND $height)
@@ -99,7 +100,7 @@ class Proxima_Asset {
 
 				$filename = $asset->id."_{$width}_{$height}_{$crop}_{$filename}";
 
-				$path = Kohana::$config->load('assets.upload_path').'/resized/'.$filename.'.'.$asset->mimetype->extension;	
+				$path = Kohana::$config->load('assets.upload_path').'/resized/'.$filename.'.'.$asset->mimetype->extension;
 			}
 		}
 		elseif ($asset->mimetype->subtype == 'application' AND $asset->mimetype->type == 'pdf' AND $width AND $height)
@@ -120,8 +121,8 @@ class Proxima_Asset {
 				->where('height', '=', $height)
 				->where('crop', '=', $crop)
 				->find();
-			
-			// If no size then create one, this is a security feature 
+
+			// If no size then create one, this is a security feature
 			// to disallow building image sizes anonymously.
 			if (!$size->loaded())
 			{
@@ -133,72 +134,78 @@ class Proxima_Asset {
 				$size->save();
 			}
 		}
-		
+
 		return $path;
 	}
-	
-	public static function image_url($asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
-	{
-		$path = self::docpath($asset, $width, $height, $crop, $full_path);
 
-		return URL::site($path, $full_path);
+	public static function image_url(Model_Asset $asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
+	{
+		return URL::site(Route::get('media/assets/resized')
+			->uri(array(
+				'id'       => $asset->id,
+				'width'    => $width,
+				'height'   => $height,
+				'crop'     => (string) (int) $crop,
+				'filename' => $asset->friendly_filename
+			)), $full_path);
 	}
-	
-	public static function image_path($asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
+
+	public static function image_path(Model_Asset $asset, $width = NULL, $height = NULL, $crop = NULL, $full_path = FALSE)
 	{
 		$path = self::docpath($asset, $width, $height, $crop);
-			
-		return ($full_path) ? DOCROOT.$path : $path;
-	}	
 
-	public static function path($asset, $full = FALSE, $resized = '')
+		return ($full_path) ? DOCROOT.$path : $path;
+	}
+
+	public static function path(Model_Asset $asset, $full = FALSE, $resized = '')
 	{
 		 $path = Kohana::$config->load('assets.upload_path').'/'.$resized.$asset->filename;
 
 		 return ($full) ? DOCROOT.$path : $path;
 	}
-	
-	public static function url($asset, $full = FALSE)
+
+	public static function url(Model_Asset $asset, $full = FALSE)
 	{
-		$url = URL::site('media/assets/'.$asset->filename);
-		
-		return ($full) ? URL::site($url, TRUE) : $url;
+		return URL::site(Route::get('media/assets')
+			->uri(array(
+				'filename' => $asset->filename
+			)), $full);
 	}
-	
+
 	public static function rotate($file_in, $degrees)
 	{
 		Image::factory( $file_in )
 		->rotate($degrees)
 		->save();
 	}
-	
+
 	public static function sharpen($file_in, $amount)
 	{
 		Image::factory( $file_in )
 		->sharpen($amount)
 		->save();
 	}
-	
+
 	public static function flip_horizontal($file_in)
 	{
 		Image::factory( $file_in )
 		->flip(Image::HORIZONTAL)
 		->save();
 	}
-	
+
 	public static function flip_vertical($file_in)
 	{
 		Image::factory( $file_in )
 		->flip(Image::HORIZONTAL)
 		->save();
 	}
-	
+
 	public static function pdfthumb($file_in, $file_out, $width, $height, $crop)
 	{
 		exec('convert -quality 85 '.$file_in.'[0] '.$file_out);
-		
+
 		static::resize($file_out, $file_out, $width, $height, $crop);
-	}	
+	}
 
 	public static function extension($filename)
 	{
