13b619570e389374141dcd8458bd2480f2b168cb
Richard Willis <willis.rh@gmail.com>
2012-01-22 23:00:02 +0000

    Modules work

--- proxima/modules/blogimport/classes/controller/admin/blogimport.php
+++ proxima/modules/blogimport/classes/controller/admin/blogimport.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Controller_Admin_BlogImport extends Controller_Admin_Base {
 
@@ -9,12 +9,12 @@ class Controller_Admin_BlogImport extends Controller_Admin_Base {
 	 */
 	public function action_index()
 	{
-		Page_View::instance()
+		$this->template
 			->title(__('Admin - Blog import'))
 			->content(
 				View_Model::factory('admin/page/blogimport/index')
 				->bind('errors', $errors)
-			); 
+			);
 
 		$data = Validation::factory($_POST)
 			->rule('blog_url', 'not_empty')
@@ -24,19 +24,19 @@ class Controller_Admin_BlogImport extends Controller_Admin_Base {
 		{
 			$result = Importer::factory($this->request->post('service'), $this->request->post())
 				->import_posts();
-	
+
 			if ($result === FALSE)
 			{
 				Message::set(Message::ERROR, __('Error loading the XML feed.'));
 
 				$this->request->redirect('admin/blogimport');
-			} 
-			
-			$message = sprintf('Saved %s of %s posts.', 
+			}
+
+			$message = sprintf('Saved %s of %s posts.',
 				$result['saved'],
 				$result['total_posts']
 			);
-				
+
 			Message::set($result > 0 ? Message::SUCCESS : Message::NOTICE, $message);
 
 			$this->request->redirect('admin/blogimport');
