d2175a491ab0701622675b98df39b2a87a8d154b
Richard Willis <willis.rh@gmail.com>
2012-02-08 21:01:27 +0000

    Passing in Config_Group to Assets constructor. Refs #39

--- proxima/core/classes/proxima/view/model/page/install/master/install.php
+++ proxima/core/classes/proxima/view/model/page/install/master/install.php
@@ -5,7 +5,7 @@ class Proxima_View_Model_Page_Install_Master_Install extends View_Model_Master {
 	public function __construct($file = NULL, array $data = NULL, Assets $assets = NULL)
 	{
 		// Load the install assets
-		$assets = new Assets(Kohana::$config->load('install.assets'));
+		$assets = new Assets(new Config_Group(Kohana::$config, 'assets', Kohana::$config->load('install.assets')));
 		$assets->group('install');
 
 		return parent::__construct($file, $data, $assets);
