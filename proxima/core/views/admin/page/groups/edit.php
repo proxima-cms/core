16509f849a85a26c1eb08db96982887a1c100f9a
Richard Willis <willis.rh@gmail.com>
2012-06-16 22:13:34 +0200

    Fixed edit group view

--- proxima/core/views/admin/page/groups/edit.php
+++ proxima/core/views/admin/page/groups/edit.php
@@ -1,59 +1,87 @@
-<div class="action-bar clear">
-	<div class="action-menu helper-right">
-		<button>Actions</button>
-		<ul>
-			<li><?php echo HTML::anchor('admin/groups/delete/'.$group->id, __('Delete group'))?></li>
-		</ul>
-	</div>
 
-	<?php echo $breadcrumbs?>
+<div class="row-fluid">
+
+  <div class="span3">
+    <div class="well sidebar-nav">
+      <ul class="nav nav-list">
+        <li class="nav-header">Actions</li>
+        <li><?php echo HTML::anchor(
+            Route::get('admin')
+              ->uri(array(
+                'controller' => 'groups',
+								'action' => 'delete'
+              )), __('Delete group'));?>
+        </li>
+        <li><?php echo HTML::anchor(
+            Route::get('admin')
+              ->uri(array(
+                'controller' => 'groups',
+              )), __('Manage groups'));?>
+        </li>
+      </ul>
+    </div><!--/.well -->
+  </div>
+
+  <div class="span9">
+
+    <div class="page-header">
+      <h1>Add group</h1>
+    </div>
+
+		<?php echo Form::open(NULL, array('class' => 'form-horizontal'))?>
+			<fieldset>
+
+				<?php echo Form::control_group(array(
+					'label' => __('Parent group'),
+					'name' => 'parent_id',
+					'type' => 'select',
+					'options' => $groups,
+					'value' => $group->parent_id
+				), $errors);?>
+				
+				<?php echo Form::control_group(array(
+					'label' => __('Name'),
+					'name' => 'name',
+					'type' => 'input',
+					'value' => $group->name
+				), $errors);?>
+
+				<div class="form-actions">
+					<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'btn btn-primary'))?>
+				</div>
+			</fieldset>
+		<?php echo Form::close()?>
+
+		<!-- USERS IN THIS GROUP -->
+		<fieldset>
+			<legend>Users in this group</legend>
+			<br />
+			<?php echo $users; ?>
+		</fieldset>
+
+		<!-- ADD USER TO THIS GROUP -->
+		<?php echo Form::open(
+			Route::get('admin')
+			->uri(array(
+				'controller' => 'groups',
+				'action' => 'adduser'
+			)) . '?return_to=' . Request::current()->uri(), array('class' => 'form-horizontal') );
+		?>
+		<?php echo Form::hidden('group_id', $group->id);?>
+		<fieldset>
+			<legend><?php echo __('Add new user to this group'); ?></legend>
+			<?php echo Form::control_group(array(
+				'label' => __('User'),
+				'name' => 'user_id',
+				'type' => 'select',
+				'options' => $users_select,
+				'value' => 0
+			), $errors);?>
+			<div class="form-actions">
+				<?php echo Form::button('save-newuser', __('Add user'), array('type' => 'submit', 'class' => 'btn btn-primary'))?>
+			</div>
+		</fieldset>
+		<?php echo Form::close(); ?>
+	</div>
 </div>
 
-<!-- GROUP -->
-<?php echo Form::open()?>
-	<fieldset>
-		<legend>User Group</legend>
-
-		<div class="field">
-			<?php echo 
-				Form::label('parent_id', __('Parent group'), NULL, $errors),
-				Form::select('parent_id', $groups, $group->parent_id, NULL, $errors)
-			?>
-		</div>
-		<div class="field">
-			<?php echo 
-				Form::label('name', __('Name'), NULL, $errors),
-				Form::input('name', $group->name, NULL, $errors)
-			?>
-		</div>
-
-		<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'ui-button save'))?>
-	</fieldset>
-<?php echo Form::close()?>
-
-<!-- USERS IN THIS GROUP -->
-<fieldset>
-	<legend>Users in this group</legend>
-	<?php echo $users; ?>
-</fieldset>
-
-<!-- ADD USER TO THIS GROUP -->
-<?php echo Form::open(
-	Route::get('admin')
-	->uri(array(
-		'controller' => 'groups',
-		'action' => 'adduser'
-	)) . '?return_to=' . Request::current()->uri() );
-?>
-<?php echo Form::hidden('group_id', $group->id);?>
-<fieldset>
-	<legend><?php echo __('Add new user to this group'); ?></legend>
-	<div class="field">
-		<?php echo 
-			Form::label('user_id', __('User'), NULL, $errors),
-			Form::select('user_id', $users_select, 0, NULL, $errors)
-		?>
-	</div>
-	<?php echo Form::button('save-newuser', __('Add user'), array('type' => 'submit', 'class' => 'ui-button save'))?>
-</fieldset>
-<?php echo Form::close(); ?>
